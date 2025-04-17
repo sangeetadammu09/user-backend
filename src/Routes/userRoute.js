391ea94f3538fbfe3bdfa0cb4a0da5155ffcf3b9
sangeetadammo09 <sangeetadammu12@gmail.com>
2025-04-17 16:48:13 +0530

    deploy code to azure

--- src/Routes/userRoute.js
+++ src/Routes/userRoute.js
@@ -7,7 +7,7 @@ router.post('/create-user', fileUpload("./Storage/images"), userController.creat
 router.get('/single-user/:id',userController.singleUser);
 router.put('/update-user/:id', fileUpload("./Storage/images"), userController.updateUser);
 router.delete('/delete-user/:id',userController.deleteUser);
-router.get('/allusers', userController.allUsers);
+router.get('/allusers', userController.allUsers); 
 router.post('/checkemailexists',userController.checkEmailExists);
 
 
